3b41e127b0ba6c0f7988f8fb6cc80697b5953b4f
jessebutterfield <jesse.butterfield@gmail.com>
2017-06-09 10:12:22 -0700

    Fix no auth

--- src/js/redux/actions/memberActions.js
+++ src/js/redux/actions/memberActions.js
@@ -2,12 +2,13 @@ import { fetchData } from './fetchDataActions'
 import { membershipApi } from '../../services/membership'
 import { MEMBER_STORE as store } from '../reducers/member'
 import { AUTH_STORE } from '../reducers/auth'
+import { USE_AUTH } from '../../config'
 
 export function fetchMember () {
   return (dispatch, getState) => {
     const authStore = getState()[AUTH_STORE]
     const user = authStore.get('user', null)
-    if (user !== null) {
+    if (!USE_AUTH || user !== null) {
       dispatch(fetchData({
         apiService: membershipApi,
         route: '/member',
